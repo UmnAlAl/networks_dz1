a58f062c3f9fe6566780db352e03b84a0488fa85
Alexander <Alex170995@yandex.ru>
2017-05-19 03:59:19 +0300

    client/server modes added

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,11 +1,19 @@
 import LZWDeCompress.LZW;
 import ShannonFanoCode.MyShannonFanoImpl;
 import io.vertx.core.Vertx;
-import io.vertx.core.http.HttpMethod;
-import io.vertx.core.http.HttpServer;
-import io.vertx.core.http.HttpServerResponse;
+import io.vertx.core.buffer.Buffer;
+import io.vertx.core.http.*;
+import io.vertx.ext.web.FileUpload;
 import io.vertx.ext.web.Router;
 import io.vertx.ext.web.handler.BodyHandler;
+
+import javax.imageio.ImageIO;
+import javax.imageio.stream.FileImageOutputStream;
+import java.awt.image.BufferedImage;
+import java.io.ByteArrayInputStream;
+import java.io.Console;
+import java.io.DataInputStream;
+import java.io.File;
 
 /**
  * Created by Installed on 15.05.2017.
@@ -13,15 +21,28 @@ import io.vertx.ext.web.handler.BodyHandler;
 public class Main {
 
     public static Vertx vertxInstance;
+    public static int port = 8089;
 
     public static void main(String [] args)
     {
         vertxInstance = Vertx.vertx();
-        StartHttpServer();
+        if(args.length >= 1) {
+            if(args[0].equals("client")) {
+                StartHttpClient();
+                System.out.println("client started");
+            } else  if (args[0].equals("server")) {
+                StartHttpServer();
+                System.out.println("server started");
+            }
+        }
+        else {
+            StartTestApiHttpServer();
+            System.out.println("server test api started");
+        }
     }
 
     //for testing via postman
-    public  static void StartHttpServer()
+    public  static void StartTestApiHttpServer()
     {
         HttpServer server = vertxInstance.createHttpServer();
         Router router = Router.router(vertxInstance);
@@ -106,7 +127,117 @@ public class Main {
             response.end();
         });
 
-        server.requestHandler(router::accept).listen(8089);
+        // NOT WORKING!
+        router.route(HttpMethod.POST, "/api/JPEGProcess/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.setChunked(true);
+            try {
+
+                String body = routingContext.getBodyAsString();
+                BufferedImage bim = ImageIO.read(new ByteArrayInputStream(body.getBytes()));
+                bim = MyImageOpsImpl.process(bim);
+                response.write(bim.toString());
+            }
+            catch (Exception ex) {
+
+            }
+            response.end();
+        });
+
+        server.requestHandler(router::accept).listen(port);
+    }
+
+    public static void StartHttpClient() {
+        HttpClient httpClient = vertxInstance.createHttpClient();
+
+        String strToCheckCRC32 = "0123456789";
+        httpClient.post(port, "localhost", "/api/CRCcheck/", httpClientResponse -> {
+            httpClientResponse.bodyHandler(buffer -> {
+                System.out.println("CRCCheck " + buffer.toString());
+            });
+        }).setChunked(true).write(strToCheckCRC32 + "|" + MyCRCImpl.countCRC32(strToCheckCRC32)).end();
+
+
+        Integer intToCheckHamming = 123456;
+        httpClient.post(port, "localhost", "/api/HammingDecrypt/", httpClientResponse -> {
+            httpClientResponse.bodyHandler(buffer -> {
+                System.out.println("HammingDecrypt " + buffer.toString());
+            });
+        }).setChunked(true).write( Integer.toString(MyHammingImpl.HammingEncode(intToCheckHamming)) ).end();
+
+
+        String strToCheckShannonFano = "abbcdbeeffffffefefeeef";
+        httpClient.post(port, "localhost", "/api/ShannonFanoDecode/", httpClientResponse -> {
+            httpClientResponse.bodyHandler(buffer -> {
+                System.out.println("ShannonFanoDecode " + buffer.toString());
+            });
+        }).setChunked(true).write( MyShannonFanoImpl.encode(strToCheckShannonFano) ).end();
+
+
+        String strToCheckLZW = "aabacababcabac";
+        httpClient.post(port, "localhost", "/api/LZWDecompress/", httpClientResponse -> {
+            httpClientResponse.bodyHandler(buffer -> {
+                System.out.println("LZWDecompress " + buffer.toString());
+            });
+        }).setChunked(true).write( LZW.compress(strToCheckLZW) ).end();
+
+        httpClient.close();
+
+    }
+
+    public static void StartHttpServer() {
+
+        HttpServer server = vertxInstance.createHttpServer();
+        Router router = Router.router(vertxInstance);
+        router.route().handler(BodyHandler.create());
+
+        router.route(HttpMethod.POST, "/api/CRCcheck/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.setChunked(true);
+            String body = routingContext.getBodyAsString();
+            if(MyCRCImpl.checkCRC32(body)) {
+                System.out.println(body + "   good CRC");
+                response.write("good");
+            }
+            else {
+                System.out.println(body + "   bad CRC");
+                response.write("bad");
+            }
+            response.end();
+        });
+
+        router.route(HttpMethod.POST, "/api/HammingDecrypt/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.setChunked(true);
+            String body = routingContext.getBodyAsString();
+            int y = Integer.parseInt(body);
+            int x = MyHammingImpl.HammingDecode(y);
+            response.write(Integer.toString(x));
+            response.end();
+            System.out.println("HammingDecrypted " + Integer.toString(x));
+        });
+
+        router.route(HttpMethod.POST, "/api/ShannonFanoDecode/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.setChunked(true);
+            String body = routingContext.getBodyAsString();
+            String decoded = MyShannonFanoImpl.decode(body);
+            response.write(decoded);
+            response.end();
+            System.out.println("ShannonFanoDecoded " + decoded);
+        });
+
+        router.route(HttpMethod.POST, "/api/LZWDecompress/").handler(routingContext -> {
+            HttpServerResponse response = routingContext.response();
+            response.setChunked(true);
+            String body = routingContext.getBodyAsString();
+            String decompressed = LZW.expand(body);
+            response.write(decompressed);
+            response.end();
+            System.out.println("LZWDecompressed " + decompressed);
+        });
+
+        server.requestHandler(router::accept).listen(port);
     }
 
 }
---
+++ src/main/java/MyImageOpsImpl.java
@@ -0,0 +1,12 @@
+import java.awt.image.BufferedImage;
+
+/**
+ * Created by Installed on 19.05.2017.
+ */
+public class MyImageOpsImpl {
+
+    public static BufferedImage process(BufferedImage bimInput) {
+        return bimInput;
+    }
+
+}
